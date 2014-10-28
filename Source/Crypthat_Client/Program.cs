cfccbadd8dc5f18f2db39fc93b9414f840e78012
Cippe <acippelletti@gmail.com>
2014-10-28 12:13:01 +0100

    Test Git da Visual Studio

--- Source/Crypthat_Client/Program.cs
+++ Source/Crypthat_Client/Program.cs
@@ -17,6 +17,7 @@ namespace Crypthat_Client
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new MainForm());
+
         }
     }
 }
