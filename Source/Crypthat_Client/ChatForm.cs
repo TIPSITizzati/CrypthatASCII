52293b0391d4baacfbf40cace57b63f562f39324
artum <jacopo.libe@gmail.com>
2014-12-07 00:25:33 +0100

    Fix Vari

---
+++ Source/Crypthat_Client/ChatForm.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using Crypthat_Common;
+using System.Runtime.InteropServices;
+
+namespace Crypthat_Client
+{
+    public partial class ChatForm : Form
+    {
+        private Identity dest;
+        private GestoreLogicoClient gestoreClient;
+        private UserList parent;
+
+        public ChatForm(UserList parent, GestoreLogicoClient currentGestore, Identity dest)
+        {
+            InitializeComponent();
+
+            this.dest = dest;
+            this.gestoreClient = currentGestore;
+            this.parent = parent;
+        }
+
+        private void ChatForm_Load(object sender, EventArgs e)
+        {
+            // Imposta testi di default
+            lblName.Text = dest.Name;
+            txtSend.Select();
+            txtSend.KeyPress += txtSend_KeyPress;
+
+            txtChat.ReadOnly = true;
+
+            this.Text = "Crypthat - Chat ( " + dest.Name + " )";
+        }
+
+        private void txtSend_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if((Keys)e.KeyChar == Keys.Enter)
+            {
+                InviaMessaggio();
+                e.Handled = true;
+            }
+        }
+
+        private void btnSend_Click(object sender, EventArgs e)
+        {
+            InviaMessaggio();
+        }
+
+        private void ChatForm_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            parent.chatAttive.Remove(dest);
+        }
+
+        private void InviaMessaggio()
+        {
+            // Se si ha del testo da inviare
+            if(!String.IsNullOrEmpty(txtSend.Text.Replace("\n", "")))
+            {
+                // Invia il messaggio
+                gestoreClient.InviaMessaggio(txtSend.Text, dest, false);
+                ScriviMessaggio(txtSend.Text, gestoreClient.Me);
+                txtSend.Clear();
+            }
+        }
+
+
+        public void ScriviMessaggio(string Messaggio, Identity Mittente)
+        {
+            txtChat.AppendText(String.Format("[{0}] {1} - {2}\n", DateTime.Now.ToShortTimeString(), Mittente.Name, Messaggio));
+            
+            // Effettua l'auto-scroll alla fine
+            txtChat.SelectionStart = txtChat.Text.Length; 
+            txtChat.ScrollToCaret();
+
+            if(!this.Focused)
+            {
+                FlashWindowEx(this);
+            }
+
+        }
+
+        public void DisabilitaChat()
+        {
+            btnSend.Enabled = false;
+            txtSend.Enabled = false;
+            ScriviMessaggio("Offline", dest);
+        }
+
+        // Parte aggiunta per avere l'icona lampeggiante copiata da http://stackoverflow.com/questions/11309827/window-application-flash-like-orange-on-taskbar-when-minimize
+        [DllImport("user32.dll")]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        static extern bool FlashWindowEx(ref FLASHWINFO pwfi);
+
+        //Flash both the window caption and taskbar button.
+        //This is equivalent to setting the FLASHW_CAPTION | FLASHW_TRAY flags. 
+        public const UInt32 FLASHW_ALL = 3;
+
+        // Flash continuously until the window comes to the foreground. 
+        public const UInt32 FLASHW_TIMERNOFG = 12;
+
+        [StructLayout(LayoutKind.Sequential)]
+        public struct FLASHWINFO
+        {
+            public UInt32 cbSize;
+            public IntPtr hwnd;
+            public UInt32 dwFlags;
+            public UInt32 uCount;
+            public UInt32 dwTimeout;
+        }
+
+        // Do the flashing - this does not involve a raincoat.
+        public static bool FlashWindowEx(Form form)
+        {
+            IntPtr hWnd = form.Handle;
+            FLASHWINFO fInfo = new FLASHWINFO();
+
+            fInfo.cbSize = Convert.ToUInt32(Marshal.SizeOf(fInfo));
+            fInfo.hwnd = hWnd;
+            fInfo.dwFlags = FLASHW_ALL | FLASHW_TIMERNOFG;
+            fInfo.uCount = UInt32.MaxValue;
+            fInfo.dwTimeout = 0;
+
+            return FlashWindowEx(ref fInfo);
+        }
+    }
+}
---
+++ Source/Crypthat_Client/UserList.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using Crypthat_Common;
+
+namespace Crypthat_Client
+{
+    public partial class UserList : Form
+    {
+        private GestoreLogicoClient gestoreClient;
+        public Dictionary<Identity, ChatForm> chatAttive; // <Persona, Form>
+
+        public UserList(Identity Me, ModalitaOperativa opMode, object param)
+        {
+            InitializeComponent();
+
+            // Impedisce la modifica del form
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+
+            // Inizializza la lista di chat attive
+            chatAttive = new Dictionary<Identity, ChatForm>();
+
+            switch (opMode)
+            {
+                case ModalitaOperativa.Rs232:
+                    gestoreClient = new GestoreLogicoClient(opMode, Me, (string)param);
+                    break;
+                case ModalitaOperativa.Sockets:
+                    gestoreClient = new GestoreLogicoClient(opMode, Me, (System.Net.IPEndPoint)param);
+                    break;
+            }
+
+            gestoreClient.OnMessaggioRicevuto += gestoreClient_OnMessaggioRicevuto;
+            gestoreClient.OnUtenteRegistrato += gestoreClient_OnUtenteRegistrato;
+            gestoreClient.OnUtenteDisconnesso += gestoreClient_OnUtenteDisconnesso;
+
+            lblUtenti.Text = "0 Utenti Connessi";
+            lblNomeUtente.Text = Me.Name;
+        }
+
+        void gestoreClient_OnUtenteDisconnesso(object sender, InterLevelArgs args)
+        {
+            // Previene azione cross-thread
+            Invoke(new Action(() =>
+            {
+                // Controlla che non sia terminata la connessione con il server
+                if((int)args.Data == -1)
+                {
+                    MessageBox.Show("Sei stato disconnesso forzatamente dal server!\nL'applicazione sarà chiusa.", "Errore di Connessione", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    Application.Exit();
+                    return;
+                }
+
+                // Rimuove l'utente dalla lista utenti
+                lsUtenti.Items.RemoveAt((int)args.Data - 1);
+                lblUtenti.Text = String.Format("{0} Utenti Connessi", lsUtenti.Items.Count);
+
+                // Se è attiva una chat con l'utente in questione
+                if(chatAttive.ContainsKey(args.Subject))
+                {
+                    chatAttive[args.Subject].DisabilitaChat();  // Disabilita la sessione
+                }
+            }));
+        }
+
+        private void gestoreClient_OnUtenteRegistrato(object sender, InterLevelArgs args)
+        {
+            // Previene azione cross-thread
+            Invoke(new Action(() =>
+            {
+                // Se l'utente aggiunto non è il server
+                if (gestoreClient.Destinatari.IndexOf(args.Subject) != 0)
+                {
+                    lsUtenti.Items.Add(args.Subject.Name);
+                    lblUtenti.Text = String.Format("{0} Utenti Connessi", lsUtenti.Items.Count - 1);
+                }
+            }));
+        }
+
+        private void gestoreClient_OnMessaggioRicevuto(object sender, InterLevelArgs args)
+        {
+            // Previene azione cross-thread
+            Invoke(new Action(() =>
+            {
+                // Visualizza il messaggio nella rispettiva chat
+                
+                // Se la chat con l'utente da cui arriva il messaggio è già attiva
+                if(chatAttive.ContainsKey(args.Subject))
+                    chatAttive[args.Subject].ScriviMessaggio(args.Data.ToString(), args.Subject);
+                else  // Sennò crea la chat relativa
+                {
+                    ChatForm nuovaChat = new ChatForm(this, gestoreClient, args.Subject);
+                    nuovaChat.Show();
+                    chatAttive.Add(args.Subject, nuovaChat);
+                    nuovaChat.ScriviMessaggio(args.Data.ToString(), args.Subject);
+                }
+            }));
+        }
+
+        private void UserList_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            // Esce sempre dall'applicazione
+            //TODO: Aggiungere chiusura concordata della connessione
+            Application.Exit();
+        }
+
+        private void lsUtenti_DoubleClick(object sender, EventArgs e)
+        {
+            if(lsUtenti.SelectedIndex != -1)
+            {
+                if (!chatAttive.ContainsKey(gestoreClient.Destinatari[lsUtenti.SelectedIndex + 1]))
+                {
+                    ChatForm nuovaChat = new ChatForm(this, gestoreClient, gestoreClient.Destinatari[lsUtenti.SelectedIndex + 1]);
+                    nuovaChat.Show();
+                    chatAttive.Add(gestoreClient.Destinatari[lsUtenti.SelectedIndex + 1], nuovaChat);
+                }
+                else
+                    chatAttive[gestoreClient.Destinatari[lsUtenti.SelectedIndex + 1]].Activate();
+            }
+        }
+    }
+}
